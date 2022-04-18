53ccff5eb15b0b5ff98013f00d64eb8bdfda2892
Andrew Scott Cohen <andrewscohen@uchicago.edu>
2022-04-18 20:53:05 +0000

    Complete

--- each_letter_count.rb
+++ each_letter_count.rb
@@ -4,3 +4,9 @@
 #  The program should print each letter in the word the number of times it appears in the word.
 
 p "Enter a word:"
+word = gets.chomp.split("")
+
+word.each do |the_letter|
+  freq = word.count(the_letter)
+  p "#{the_letter} appears #{freq} times"
+end
--- each_spell_word.rb
+++ each_spell_word.rb
@@ -3,4 +3,8 @@
 # Asks your user to enter any word and have it spelled out letter by letter.  
 
 p "Enter a word for me to spell:"
+word = gets.chomp.split("")
 
+word.each do |the_letter|
+  p the_letter
+end
